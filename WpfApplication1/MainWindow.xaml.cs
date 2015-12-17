5a4dbd6a6dbb1fc05915a0d6235abf7b8fc9b3a9
sbace <steffenbamlund@gmail.com>
2015-12-17 20:32:55 +0100

    First commit

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ipconfig_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("net", "help");
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+            psi.CreateNoWindow = true;
+
+            var proc = Process.Start(psi);
+
+            string s = proc.StandardOutput.ReadToEnd();
+
+            textBox.Text = s;
+
+        }
+
+        /*private void richTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }*/
+    }
+}
