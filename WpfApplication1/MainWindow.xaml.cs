2f2a87ccb14118c597257ea74b71b1ccdb24e331
sbace <steffenbamlund@gmail.com>
2015-12-31 20:39:31 +0100

    del2

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Net.NetworkInformation;
 
 namespace WpfApplication1
 {
@@ -28,7 +29,7 @@ namespace WpfApplication1
 
         private void Ipconfig_Click(object sender, RoutedEventArgs e)
         {
-            ProcessStartInfo psi = new ProcessStartInfo("net", "help");
+            ProcessStartInfo psi = new ProcessStartInfo("ipconfig");
             psi.UseShellExecute = false;
             psi.RedirectStandardOutput = true;
             psi.CreateNoWindow = true;
@@ -41,8 +42,45 @@ namespace WpfApplication1
 
         }
 
-        /*private void richTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void ping_Click(object sender, RoutedEventArgs e)
         {
+            Ping p = new Ping();
+            PingReply r;
+            string s;
+            s = ipa.Text;
+            r = p.Send(s);
+            try
+            {
+                if (r.Status == IPStatus.Success)
+                {
+                    textBox.Text = "Ping to " + s.ToString() +/* "[" + r.Address.ToString() + "]" +*/ " Successful"
+                       + " Response delay = " + r.RoundtripTime.ToString() + " ms" + "\n";
+                }
+            }
+            catch
+            {
+
+            }
+            
+        }
+/*
+        private void textBox1_Validated(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ipa.Text) || ipa.Text == "")
+            {
+                MessageBox.Show("Please use valid IP or web address!!");
+            }
+        }*/
+        /*private void ipa_Validated(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ipa.Text) || ipa.Text == "")
+            {
+                MessageBox.Show("Skriv lige en valid adresse!!");
+            }
+        }
+        */
+        /*private void richTextBox_TextChanged(object sender, TextChangedEventArgs e)
+       
 
         }*/
     }
